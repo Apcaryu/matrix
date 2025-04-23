5302fa6cb73cf9eb0636c27b1a9b2fb871b8593b
Apcaryu <antoinepellegrin@outlook.fr>
2025-04-23 17:56:07 +0200

    fix test for exercise05

--- Exercice05/test.cpp
+++ Exercice05/test.cpp
@@ -11,25 +11,25 @@ int main() {
 	}
 	{
 		Vector u(std::vector<float>{1.f, 0.f});
-		Vector v(std::vector<float>{1.f, 0.f});
+		Vector v(std::vector<float>{0.f, 1.f});
 		std::cout << angle_cos(u, v) << std::endl;
 		// 0.0
 	}
 	{
-		Vector u(std::vector<float>{1.f, 0.f});
-		Vector v(std::vector<float>{1.f, 0.f});
+		Vector u(std::vector<float>{-1.f, 1.f});
+		Vector v(std::vector<float>{1.f, -1.f});
 		std::cout << angle_cos(u, v) << std::endl;
 		// -1.0
 	}
 	{
-		Vector u(std::vector<float>{1.f, 0.f});
-		Vector v(std::vector<float>{1.f, 0.f});
+		Vector u(std::vector<float>{2.f, 1.f});
+		Vector v(std::vector<float>{4.f, 2.f});
 		std::cout << angle_cos(u, v) << std::endl;
 		// 1.0
 	}
 	{
-		Vector u(std::vector<float>{1.f, 0.f});
-		Vector v(std::vector<float>{1.f, 0.f});
+		Vector u(std::vector<float>{1.f, 2.f, 3.f});
+		Vector v(std::vector<float>{4.f, 5.f, 6.f});
 		std::cout << angle_cos(u, v) << std::endl;
 		// 0.974631846
 	}
